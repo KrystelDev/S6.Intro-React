1569f81f8b80e8c41106bdacb007710cf07df279
Kryste Rodriguez Castejon <krodcas@MacBook-Pro-de-Kryste.local>
2021-10-26 00:19:32 +0200

    Exerici 1

---
+++ consells/src/components/escena/Escena.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const escena = () => {
+    return ( 
+        <p>"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"</p>
+     );
+}
+ 
+export default escena;
