5a17d1530b9abb3ded49592bc8785ba04837274d
Kryste Rodriguez Castejon <krodcas@MacBook-Pro-de-Kryste.local>
2021-11-06 23:50:25 +0100

    Level 1 finished

--- consells/src/components/Desplacar.js
+++ consells/src/components/Desplacar.js
@@ -1,30 +1,48 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Desplacar= () => {
 
-    const Button = styled.button`
-    border-radius: 1px;
-    border: 1px solid grey;
-    background: gainsboro;
-    width: 50%;
-    height: 100%;
-    font-size: larger;
-    `;
+const Button = styled.button`
+  border-radius: 1px;
+  border: 1px solid grey;
+  background-color: gainsboro;
+  width: 50%;
+  height: 100%;
+  font-size: larger;
+`;
 
-    const Container = styled.div`
-    text-align: center;
-    size: 3px;
-    height: 10vh;
-    margin: 2px 2px 0px 2px;
-    `;
+const Container = styled.div`
+  text-align: center;
+  size: 3px;
+  height: 10vh;
+  margin: 2px 2px 0px 2px;
+`;
 
-    return ( 
-        <Container>
-            <Button onClick=''>Anterior</Button>
-            <Button onClick=''>Següent</Button>
-        </Container>
-     );
-}
- 
+const Desplacar = ({ sumarPosicion, restarPosicion }) => {
+  return (
+    <Container>
+      <Button
+        type="button"
+        onClick={() => {
+          restarPosicion();
+        }}
+      >
+        Anterior
+      </Button>
+      {/* <Button 
+        type="button" 
+        onClick={() => {
+            sumarPosicion();
+        }}
+      >
+        Següent
+      </Button> */}
+      <Button type="button" onClick={sumarPosicion}>
+        Següent
+      </Button>
+    </Container>
+  );
+  
+};
+
 export default Desplacar;
--- consells/src/components/escena/Escena.js
+++ consells/src/components/escena/Escena.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React from "react";
 // import './escena.css'; + div className="escena"..
-import styled from 'styled-components';
 
+const seleccionada = {
+  border: `2px solid black`,
+  borderRadius: '50px',
+  margin: '20px 20px 0px 20px',
+  textAlign: 'center',
+  fontSize: 'small',
+  backgroundColor: '#f5b7b1',
+};
 
-const Escena = ({frase}) => {
+const noSeleccionada = {
+  border: `2px solid black`,
+  borderRadius: '50px',
+  margin: '20px 20px 0px 20px',
+  textAlign: 'center',
+  fontSize: 'small',
+  opacity: '0.6'
+};
 
-    const Escenes = styled.div`
-        border: 2px solid black;
-        border-radius: 50px;
-        margin: 20px 20px 0px 20px;
-        text-align: center;
-        font-size: small;
-    `;
+const Escena = ({ index, consell, posicion, seleccionarEscena }) => {
+  console.log("index: " + index + " posicion: " + posicion);
 
-    return ( 
-        <Escenes>
-            <p>{frase}</p>
-        </Escenes>
-     );
-}
- 
+  return (
+    <div
+      key={index}
+      posicion={posicion}
+      index={index}
+      style={seleccionarEscena !== 0 ? seleccionada : noSeleccionada}
+    >
+      <p>{consell}</p>
+    </div>
+  );
+};
+
 export default Escena;
